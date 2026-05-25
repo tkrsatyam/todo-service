c0ccc8405cd534cee02e24b8159e22f31a85ba48
satyamk7 <satyamk.1107@gmail.com>
2026-05-25 17:28:49 +0530

    feat: add Todo entity with JPA mapping

---
+++ src/main/java/com/github/tkrsatyam/todoservice/model/Todo.java
@@ -0,0 +1,36 @@
+package com.github.tkrsatyam.todoservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todos")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private boolean completed = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
